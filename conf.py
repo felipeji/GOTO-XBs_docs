53da90d5b3d17b0302e977f98ef276d7a04fe394
Felipe Jimenez-Ibarra <felipejimenezibarra@gmail.com>
2023-09-23 18:57:08 +0100

    dalee

--- conf.py
+++ conf.py
@@ -26,5 +26,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
-html_output_path = '$READTHEDOCS_OUTPUT/html'
-
+html_output_path = '_build/html'
